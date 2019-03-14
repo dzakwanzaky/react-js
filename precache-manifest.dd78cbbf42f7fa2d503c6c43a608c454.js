834ce037236cc837b849202bbb22dcd22f4f0667
dzakwanzaky <dzakwanzaky48@gmail.com>
2019-03-14 08:38:32 +0700

    Updates

---
+++ precache-manifest.dd78cbbf42f7fa2d503c6c43a608c454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6ee5f688a537260c913d",
+    "url": "/react-js/static/js/runtime~main.1d73b6fc.js"
+  },
+  {
+    "revision": "75e23ec4fc5874b2fca3",
+    "url": "/react-js/static/js/main.24de7389.chunk.js"
+  },
+  {
+    "revision": "86208f7e04e8580d2c29",
+    "url": "/react-js/static/js/2.e8ef8b6b.chunk.js"
+  },
+  {
+    "revision": "75e23ec4fc5874b2fca3",
+    "url": "/react-js/static/css/main.fa1412ca.chunk.css"
+  },
+  {
+    "revision": "709915f0e6f4e87def3df84327f9dfdd",
+    "url": "/react-js/index.html"
+  }
+];
